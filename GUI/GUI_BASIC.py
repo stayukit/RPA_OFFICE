8e9df155b403b3c90d59b22d81bbb26ab0367fa5
kuntew <staryu.thp@hotmail.com>
2021-07-16 15:57:05 +0700

    First commit

--- GUI/GUI_BASIC.py
+++ GUI/GUI_BASIC.py
@@ -1,1 +1,22 @@
+from tkinter import *
 
+GUI = Tk()
+GUI.title('My app')
+GUI.geometry('500x400')
+
+L1 = Label(GUI, text='Header')
+L1.pack()
+
+E1 = Entry(GUI)
+E1.pack()
+
+L2 = Label(GUI, text='Note')
+L2.pack()
+
+E2 = Entry(GUI)
+E2.pack()
+
+B1 = Button(GUI, text='Save')
+B1.pack()
+
+GUI.mainloop()
